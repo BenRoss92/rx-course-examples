4cb2103a9e10f6c9086a342f2663c6aef733d9af
Ben Ross <benjamin.ross@youview.com>
2019-09-14 18:33:41 +0100

    'map' & 'filter' operators

--- src/map-filter.js
+++ src/map-filter.js
@@ -1,25 +1,34 @@
-import {add} from "./helpers"
-import {interval} from "rxjs";
-import {map, filter, take} from "rxjs/operators";
+import { add } from "./helpers";
 
+import { interval } from "rxjs";
+import { take, filter, map } from "rxjs/operators";
 
-const numbers = ['zero', 'one', 'two', 'three', 'four'];
-const counter = interval(1000).pipe( take(4) );
+/**
+ * Task
+ * 
+ * Create a counter that every second will count from 0-3 sequentially (and then finish).
+ * 
+ * Take only the even count values. With these event count values, find the items in the following array, and add them as list items to the DOM:
+ * ```js
+ * ['zero', 'one', 'two', 'three', 'four'];
+ * ```
+ */
 
-counter.pipe(
-  filter(
-      (value) => {
-          return value % 2 === 0
-      }
-  ),
-  map(
-      (value)=>{
-          return numbers[value]
-      }
-  )
-).subscribe(
-    add.li
-)
-
-
+const words = ['zero', 'one', 'two', 'three', 'four'];
+ 
+const count$ = interval(1000)
+    .pipe(
+        take(4),
+    );
+ 
+count$
+    .pipe(
+        filter(number => number % 2 === 0)
+    )
+    .pipe(
+        map((number) => words[number])
+    )
+    .subscribe((count) => {
+        add.li(count);
+    });
 
